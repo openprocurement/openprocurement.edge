cae193306c3f975318611dfb4bdfcf8e9889a604
Leits <leits@quintagroup.org>
2016-09-23 16:05:00 +0300

    Clean up tests

--- openprocurement/litepublic/tests/base.py
+++ openprocurement/litepublic/tests/base.py
@@ -11,196 +11,12 @@ from uuid import uuid4
 from openprocurement.api.models import SANDBOX_MODE
 from openprocurement.api.utils import VERSION
 from openprocurement.api.design import sync_design
-from openprocurement.api.tests.base import BaseWebTest
+from openprocurement.api.tests.base import BaseWebTest, test_tender_data, test_bids, test_organization, test_lots
 
 try:
     from openprocurement.contracting.api.tests.base import test_contract_data
 except ImportError:
     test_contract_data = None
-
-now = datetime.now()
-test_organization = {
-    "name": "Державне управління справами",
-    "identifier": {
-        "scheme": "UA-EDR",
-        "id": "00037256",
-        "uri": "http://www.dus.gov.ua/"
-    },
-    "address": {
-        "countryName": "Україна",
-        "postalCode": "01220",
-        "region": "м. Київ",
-        "locality": "м. Київ",
-        "streetAddress": "вул. Банкова, 11, корпус 1"
-    },
-    "contactPoint": {
-        "name": "Державне управління справами",
-        "telephone": "0440000000"
-    }
-}
-test_procuringEntity = test_organization.copy()
-test_tender_data = {
-    "title": "футляри до державних нагород",
-    "procuringEntity": test_procuringEntity,
-    "value": {
-        "amount": 500,
-        "currency": "UAH"
-    },
-    "minimalStep": {
-        "amount": 35,
-        "currency": "UAH"
-    },
-    "items": [
-        {
-            "description": "футляри до державних нагород",
-            "classification": {
-                "scheme": "CPV",
-                "id": "44617100-9",
-                "description": "Cartons"
-            },
-            "additionalClassifications": [
-                {
-                    "scheme": "ДКПП",
-                    "id": "17.21.1",
-                    "description": "папір і картон гофровані, паперова й картонна тара"
-                }
-            ],
-            "unit": {
-                "name": "item",
-                "code": "44617100-9"
-            },
-            "quantity": 5,
-            "deliveryDate": {
-                "startDate": (now + timedelta(days=2)).isoformat(),
-                "endDate": (now + timedelta(days=5)).isoformat()
-            },
-            "deliveryAddress": {
-                "countryName": "Україна",
-                "postalCode": "79000",
-                "region": "м. Київ",
-                "locality": "м. Київ",
-                "streetAddress": "вул. Банкова 1"
-            }
-        }
-    ],
-    "enquiryPeriod": {
-        "endDate": (now + timedelta(days=7)).isoformat()
-    },
-    "tenderPeriod": {
-        "endDate": (now + timedelta(days=14)).isoformat()
-    },
-    "procurementMethodType": "belowThreshold",
-}
-if SANDBOX_MODE:
-    test_tender_data['procurementMethodDetails'] = 'quick, accelerator=1440'
-test_features_tender_data = test_tender_data.copy()
-test_features_item = test_features_tender_data['items'][0].copy()
-test_features_item['id'] = "1"
-test_features_tender_data['items'] = [test_features_item]
-test_features_tender_data["features"] = [
-    {
-        "code": "OCDS-123454-AIR-INTAKE",
-        "featureOf": "item",
-        "relatedItem": "1",
-        "title": "Потужність всмоктування",
-        "title_en": "Air Intake",
-        "description": "Ефективна потужність всмоктування пилососа, в ватах (аероватах)",
-        "enum": [
-            {
-                "value": 0.1,
-                "title": "До 1000 Вт"
-            },
-            {
-                "value": 0.15,
-                "title": "Більше 1000 Вт"
-            }
-        ]
-    },
-    {
-        "code": "OCDS-123454-YEARS",
-        "featureOf": "tenderer",
-        "title": "Років на ринку",
-        "title_en": "Years trading",
-        "description": "Кількість років, які організація учасник працює на ринку",
-        "enum": [
-            {
-                "value": 0.05,
-                "title": "До 3 років"
-            },
-            {
-                "value": 0.1,
-                "title": "Більше 3 років, менше 5 років"
-            },
-            {
-                "value": 0.15,
-                "title": "Більше 5 років"
-            }
-        ]
-    }
-]
-test_bids = [
-    {
-        "tenderers": [
-            test_organization
-        ],
-        "value": {
-            "amount": 469,
-            "currency": "UAH",
-            "valueAddedTaxIncluded": True
-        }
-    },
-    {
-        "tenderers": [
-            test_organization
-        ],
-        "value": {
-            "amount": 479,
-            "currency": "UAH",
-            "valueAddedTaxIncluded": True
-        }
-    }
-]
-test_lots = [
-    {
-        'title': 'lot title',
-        'description': 'lot description',
-        'value': test_tender_data['value'],
-        'minimalStep': test_tender_data['minimalStep'],
-    }
-]
-test_features = [
-    {
-        "code": "code_item",
-        "featureOf": "item",
-        "relatedItem": "1",
-        "title": "item feature",
-        "enum": [
-            {
-                "value": 0.01,
-                "title": "good"
-            },
-            {
-                "value": 0.02,
-                "title": "best"
-            }
-        ]
-    },
-    {
-        "code": "code_tenderer",
-        "featureOf": "tenderer",
-        "title": "tenderer feature",
-        "enum": [
-            {
-                "value": 0.01,
-                "title": "good"
-            },
-            {
-                "value": 0.02,
-                "title": "best"
-            }
-        ]
-    }
-]
 
 test_award = {'suppliers': [test_organization], 'status': 'pending', 'bid_id': ''}
 
@@ -213,7 +29,7 @@ test_document = {
     'format': 'application/msword',
 }
 
-test_auction_data = deepcopy(test_tender_data)
+test_auction_data = test_tender_data
 test_auction_bids = deepcopy(test_bids)
 for bid in test_auction_bids:
     bid['value']['amount'] = bid['value']['amount'] * bid['value']['amount']
