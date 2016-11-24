914ff15db97401ec877951a18682eaf3aaa8fc1d
VDigitall <vitalldh@gmail.com>
2016-11-24 13:23:48 +0200

    Gevent Bridge

--- openprocurement/edge/databridge.py
+++ openprocurement/edge/databridge.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from gevent import monkey
 monkey.patch_all()
 
@@ -14,13 +15,26 @@ import argparse
 from yaml import load
 from urlparse import urljoin
 from couchdb import Database, Session, ResourceNotFound
-from openprocurement_client.sync import get_tenders
+from openprocurement_client.sync import get_items
 from openprocurement_client.client import TendersClient
 import errno
 from socket import error
 from requests.exceptions import ConnectionError, MissingSchema
+import gevent.pool
+from gevent import spawn, sleep, idle
+from gevent.queue import Queue, Empty
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_CLIENTS_COUNT = 5
+DEFAULT_WORKERS_COUNT = 3
+DEFAULT_RETRY_WORKERS_COUNT = 2
+DEFAULT_FILTER_WORKERS_COUNT = 1
+DEFALUT_RETRY_TIMEOUT = 5
+DEFAULT_WORKERS_SLEEP = 5
+DEFAULT_WATCH_INTERVAL = 10
+DEFAULT_RETRIES_COUNT = 10
+DEFAULT_RESOURCE = 'tenders'
 
 class DataBridgeConfigError(Exception):
     pass
@@ -34,11 +48,32 @@ class EdgeDataBridge(object):
         self.config = config
         self.api_host = self.config_get('tenders_api_server')
         self.api_version = self.config_get('tenders_api_version')
+        self.resource = self.config_get('resource') or DEFAULT_RESOURCE
         self.retrievers_params = self.config_get('retrievers_params')
+        self.clients_count = self.config_get('clients_count') or DEFAULT_CLIENTS_COUNT
+        self.workers_count = self.config_get('workers_count') or DEFAULT_WORKERS_COUNT
+        self.filter_workers_count = self.config_get('filter_workers_count') or DEFAULT_FILTER_WORKERS_COUNT
+        self.retry_workers_count = self.config_get('retry_workers_count') or DEFAULT_RETRY_WORKERS_COUNT
+        self.retry_default_timeout = self.config_get('retry_default_timeout') or DEFALUT_RETRY_TIMEOUT
+        self.worker_sleep = self.config_get('worker_sleep') or DEFAULT_WORKERS_SLEEP
+        self.watch_interval = self.config_get('watch_interval') or DEFAULT_WATCH_INTERVAL
+        self.retries_count = self.config_get('retries_count') or DEFAULT_RETRIES_COUNT
+
+        self.workers_pool = gevent.pool.Pool(self.workers_count)
+        self.filter_workers_pool = gevent.pool.Pool(self.filter_workers_count)
+        self.retry_workers_pool = gevent.pool.Pool(self.retry_workers_count)
+        self.items_queue = Queue()
+        self.clients_queue = Queue()
+        self.retry_items_queue = Queue()
+
+        self.servers_id = []
+        self.log_dict = {}
+        self.update_seq = 0
+        self.doc_count = 0
 
         try:
             self.client = TendersClient(host_url=self.api_host,
-                api_version=self.api_version, key=''
+                api_version=self.api_version, key='', resource=self.resource
             )
         except MissingSchema:
             raise DataBridgeConfigError('In config dictionary empty or missing \'tenders_api_server\'')
@@ -52,7 +87,9 @@ class EdgeDataBridge(object):
         self.db = Database(self.couch_url,
                            session=Session(retry_delays=range(10)))
         try:
-            self.db.info()
+            db_info = self.db.info()
+            self.doc_count = db_info['doc_count']
+            self.update_seq = db_info['update_seq']
         except ResourceNotFound:
             error_message = "Database with name '" + self.config_get('public_db') + "' doesn\'t exist"
             raise DataBridgeConfigError(error_message)
@@ -72,41 +109,250 @@ class EdgeDataBridge(object):
             raise DataBridgeConfigError('In config dictionary missed section \'main\'')
 
 
-    def get_teders_list(self):
-        for item in get_tenders(host=self.api_host, version=self.api_version,
-                                key='', extra_params={'mode': '_all_'},
-                                retrievers_params=self.retrievers_params):
+    def get_items_list(self):
+        for item in get_items(host=self.api_host, version=self.api_version,
+                              key='', extra_params={'mode': '_all_'},
+                              retrievers_params=self.retrievers_params,
+                              resource=self.resource):
             yield (item["id"], item["dateModified"])
 
-    def save_tender_in_db(self, tender_id, date_modified):
-        tender_doc = self.db.get(tender_id)
-        if tender_doc:
-            if tender_doc['dateModified'] == date_modified:
-                return
-        tender = self.client.get_tender(tender_id).get('data')
-        if tender:
-            tender['_id'] = tender_id
-            tender['doc_type'] = 'Tender'
-            if tender_doc:
-                tender['_rev'] = tender_doc['_rev']
-                logger.info('Update tender {} '.format(tender_id))
-            else:
-                logger.info('Save tender {} '.format(tender_id))
-            try:
-                self.db.save(tender)
-            except Exception as e:
-                logger.info('Saving tender {} fail with error {}'.format(tender_id, e.message),
-                    extra={'MESSAGE_ID': 'edge_bridge_fail_save_in_db'})
+    def get_db_activity(self):
+        """
+        Get database activity. Count writed and updated documents.
+
+        :returns:
+            db_activity(dict): Extra params of query
+            :param:
+                doc_count(int): Count writed documents from previous check
+                update_seq(int): Count updated documents from previous check
+
+        """
+        try:
+            db_info = self.db.info()
+            db_activity = {
+                'doc_count': db_info['doc_count']-self.doc_count,
+                'update_seq': db_info['update_seq']-self.update_seq
+            }
+            self.update_seq = db_info['update_seq']
+            self.doc_count = db_info['doc_count']
+        except Exception as e:
+            print e
+            db_activity = {
+                'doc_count': -1,
+                'update_seq': -1
+            }
+        return db_activity
+
+    def add_to_retry_queue(self, item):
+        """
+        Add item to retry queue.
+
+        :param:
+            item(dict): Extra params of query
+            :param:
+                item_id(str): Item id
+                dateModified(datetime): Date modefied item
+                timeout(int): Seconds to lock item in retry queue mulitplied on 2
+                retries_count(int): Count retry to execute item
+
+        """
+        self.retry_items_queue.put({
+            'item_id': item['item_id'],
+            'dateModified': item['dateModified'],
+            'timeout': item['timeout'] * 2,
+            'retries_count': item['retries_count'] + 1
+            }, timeout=item['timeout'] * 2)
+        logger.info('Put {} {} to \'retries_queue\''.format(
+            self.resource[:-1], item['item_id']
+        ))
+
+    def items_filter(self, item_id, date_modified):
+        """
+        Filter for items. Filtered by date_modified and existing document.
+
+        :param:
+            item_id(str): Item id
+            dateModified(datetime): Date modefied item
+        :returns:
+            Put to queue filtered item.
+
+        """
+        try:
+            item_doc = self.db.get(item_id)
+        except Exception:
+            self.clients_queue.put(client)
+            self.add_to_retry_queue({
+                'item_id': item_id,
+                'dateModified': date_modified,
+                'timeout': self.retry_default_timeout,
+                'retries_count': 1
+            }, self.retry_default_timeout)
+        if item_doc:
+            if item_doc['dateModified'] != date_modified:
+                self.items_queue.put((item_id, date_modified, item_doc['_rev']))
         else:
-            logger.info('Tender {} not found'.format(tender_id))
+            self.items_queue.put((item_id, date_modified, None))
+
+    def save_item_in_db(self):
+        while 1:
+            try:
+                client = self.clients_queue.get(timeout=0)
+                logger.info('Got client {}'.format(
+                    client.session.cookies['AWSELB']
+                ))
+            except Empty:
+                sleep(self.worker_sleep)
+                continue
+            try:
+                item_id, date_modified, rev = self.items_queue.get(timeout=0)
+            except Empty:
+                self.clients_queue.put(client)
+                sleep(self.worker_sleep)
+                continue
+            try:
+                item = client.get_tender(item_id).get('data')
+            except Exception:
+                self.add_to_retry_queue({
+                    'item_id': item_id,
+                    'dateModified': date_modified,
+                    'timeout': self.retry_default_timeout,
+                    'retries_count': 1
+                }, self.retry_default_timeout)
+                self.clients_queue.put(client)
+                continue
+            if item:
+                item['_id'] = item_id
+                item['doc_type'] = self.resource[:-1].title()
+                if rev is not None:
+                    item['_rev'] = rev
+                    logger.info('Update {} {} '.format(self.resource[:-1], item_id))
+                else:
+                    logger.info('Save {} {} '.format(self.resource[:-1], item_id))
+                try:
+                    self.db.save(item)
+                    self.clients_queue.put(client)
+                except Exception as e:
+                    logger.info('Saving {} {} fail with error {}'.format(self.resource[:-1], item_id, e.message),
+                        extra={'MESSAGE_ID': 'edge_bridge_fail_save_in_db'})
+                    self.add_to_retry_queue({
+                        'item_id': item_id,
+                        'dateModified': date_modified,
+                        'timeout': self.retry_default_timeout,
+                        'retries_count': 1
+                    }, self.retry_default_timeout)
+                    self.clients_queue.put(client)
+            else:
+                logger.info('{} {} not found'.format(self.resource[:-1].title(),
+                                                     item_id))
+                self.clients_queue.put(client)
+
+    def retry_save_item_in_db(self):
+        while 1:
+            try:
+                client = self.clients_queue.get(timeout=0)
+                logger.info('Retry saving {} with client {}'.format(
+                    self.resource[:-1], client.session.cookies['AWSELB']
+                ))
+            except Empty:
+                sleep(self.worker_sleep)
+                continue
+            try:
+                retry_item = self.retry_items_queue.get(timeout=0)
+            except Empty:
+                self.clients_queue.put(client)
+                sleep(self.worker_sleep)
+                continue
+            try:
+                item = client.get_tender(retry_item['item_id']).get('data')
+            except Exception:
+                self.clients_queue.put(client)
+                if retry_item['retries_count'] >= self.retries_count:
+                    logger.info('{} {} reached limit retries count {} and' \
+                                ' droped from retry_queue.'.format(
+                        self.resource[:-1].title(), retry_item['item_id'],
+                        self.retries_count
+                    ))
+                else:
+                    self.add_to_retry_queue(retry_item, retry_item['timeout'])
+                continue
+            try:
+                item_doc = self.db.get(item_id)
+            except Exception:
+                self.clients_queue.put(client)
+                self.add_to_retry_queue(retry_item, retry_item['timeout'])
+            if item_doc:
+                if item_doc['dateModified'] == date_modified:
+                    self.clients_queue.put(client)
+                    continue
+            if item:
+                item['_id'] = retry_item['item_id']
+                item['doc_type'] = self.resource[:-1].title()
+                if item_doc:
+                    item['_rev'] = item_doc['_rev']
+                    logger.info('Update {} {} '.format(self.resource[:-1].title(),
+                                                       retry_item['item_id']))
+                else:
+                    logger.info('Save {} {} '.format(self.resource[:-1].title(),
+                                                     retry_item['item_id']))
+                try:
+                    self.db.save(item)
+                    self.clients_queue.put(client)
+                except Exception as e:
+                    logger.info('Saving {} {} fail with error {}'.format(
+                        self.resource[:-1], retry_item['tender_id'], e.message
+                        ), extra={'MESSAGE_ID': 'edge_bridge_fail_save_in_db'})
+                    self.clients_queue.put(client)
+                    self.add_to_retry_queue(retry_item, retry_item['timeout'])
+            else:
+                logger.info('{} {} not found'.format(self.resource[:-1].title(),
+                                                     retry_item['item_id']))
+                self.clients_queue.put(client)
+
+    def fill_clients_queue(self):
+        while len(self.servers_id) < self.clients_count:
+            client = TendersClient(host_url=self.api_host,
+                api_version=self.api_version, key='', resource=self.resource
+            )
+            if client.session.cookies['AWSELB'] not in self.servers_id:
+                self.servers_id.append(client.session.cookies['AWSELB'])
+                self.clients_queue.put(client)
+                logger.info('Started client for server {}'.format(
+                    client.session.cookies['AWSELB']))
+            else:
+                del client
+
+    def fill_items_queue(self):
+        for item_id, date_modified in self.get_items_list():
+            self.items_filter(item_id, date_modified)
+
+    def gevent_watcher(self):
+        db_activity = self.get_db_activity()
+        self.log_dict['items_queue_size'] = self.items_queue.qsize()
+        self.log_dict['workers_count'] = self.workers_count - self.workers_pool.free_count()
+        self.log_dict['filter_workers_count'] = self.filter_workers_count - self.filter_workers_pool.free_count()
+        self.log_dict['retry_workers_count'] = self.retry_workers_count - self.retry_workers_pool.free_count()
+        self.log_dict['free_clients'] = self.clients_queue.qsize()
+        self.log_dict['write_documents'] = db_activity['doc_count']
+        self.log_dict['update_documents'] = db_activity['update_seq']
+        for i in xrange(0, self.workers_pool.free_count()):
+            self.workers_pool.spawn(self.save_item_in_db)
+        for i in xrange(0, self.retry_workers_pool.free_count()):
+            self.retry_workers_pool.spawn(self.retry_save_item_in_db)
+        print '================================================================'
+        print self.log_dict
+        print '================================================================'
 
     def run(self):
         logger.info('Start Edge Bridge',
                     extra={'MESSAGE_ID': 'edge_bridge_start_bridge'})
         logger.info('Start data sync...',
                     extra={'MESSAGE_ID': 'edge_bridge__data_sync'})
-        for tender_id, date_modified in self.get_teders_list():
-            self.save_tender_in_db(tender_id, date_modified)
+        # self.fill_queue_pool.spawn(self.get_teders_list)
+        self.fill_clients_queue()
+        self.filter_workers_pool.spawn(self.fill_items_queue)
+        while 1:
+            self.gevent_watcher()
+            sleep(self.watch_interval)
 
 
 def main():
--- openprocurement/edge/tests/databridge.py
+++ openprocurement/edge/tests/databridge.py
@@ -23,7 +23,16 @@ class TestEdgeDataBridge(TenderBaseWebTest):
             'tenders_api_version': "0",
             'public_tenders_api_server': 'https://lb.api-sandbox.openprocurement.org',
             'couch_url': 'http://localhost:5984',
-            'public_db': 'public_db'
+            'public_db': 'public_db',
+            'queue_size': 101,
+            'clients_count': 3,
+            'workers_count': 3,
+            'retry_workers_count': 2,
+            'filter_workers_count': 2,
+            'retry_workers_count': 2,
+            'retry_default_timeout': 5,
+            'worker_sleep': 5,
+            'watch_interval': 10
         },
         'version': 1
     }
@@ -141,94 +150,93 @@ class TestEdgeDataBridge(TenderBaseWebTest):
             test_config['main']['public_db']
 
 
-    def test_save_tender_in_db(self):
-        log_string = io.BytesIO()
-        stream_handler = logging.StreamHandler(log_string)
-        logger.addHandler(stream_handler)
+    # def test_save_tender_in_db(self):
+    #     log_string = io.BytesIO()
+    #     stream_handler = logging.StreamHandler(log_string)
+    #     logger.addHandler(stream_handler)
+    #
+    #     bridge = EdgeDataBridge(self.config)
+    #     mock_tender = {'data': test_tender_data}
+    #     bridge.client.get_tender = MagicMock(return_value=mock_tender)
+    #
+    #     # Save tender
+    #
+    #     tid = uuid.uuid4().hex
+    #     t_date_modified = datetime.datetime.utcnow().isoformat()
+    #     mock_tender['data']['dateModified'] = t_date_modified
+    #     bridge.save_tender_in_db(tid, t_date_modified)
+    #     x = log_string.getvalue().split('\n')
+    #     self.assertEqual(x[1].strip(), 'Save tender ' + tid)
+    #     tender_in_db = bridge.db.get(tid)
+    #     self.assertEqual(tender_in_db.id, tid)
+    #
+    #     # Tender exist in db and not modified
+    #     result = bridge.save_tender_in_db(tid, t_date_modified)
+    #     self.assertEqual(result, None)
+    #
+    #     # Update tender
+    #     t_date_modified = datetime.datetime.utcnow().isoformat()
+    #     mock_tender['data']['dateModified'] = t_date_modified
+    #     bridge.save_tender_in_db(tid, t_date_modified)
+    #     x = log_string.getvalue().split('\n')
+    #     self.assertEqual(x[2].strip(), 'Update tender ' + tid)
+    #     updated_tender = bridge.db.get(tid)
+    #     self.assertEqual(updated_tender['dateModified'], unicode(t_date_modified))
+    #
+    #     # Tender not found
+    #     bridge.client.get_tender = MagicMock(return_value=test_tender_data)
+    #     bridge.save_tender_in_db(tid, datetime.datetime.utcnow().isoformat())
+    #     x = log_string.getvalue().split('\n')
+    #     self.assertEqual(x[3].strip(), 'Tender ' + tid + ' not found')
+    #     bridge.db.delete(updated_tender)
+    #
+    #     # Saving tender with exception
+    #     bridge.client.get_tender = MagicMock(return_value=mock_tender)
+    #     bridge.config['main']['couch_url'] = ''
+    #     bridge.config['main']['public_db'] = ''
+    #     bridge.db = Database('bridge.couch_url',
+    #                         session=Session(retry_delays=range(10)))
+    #     new_mock_tender = mock_tender
+    #     new_mock_tender['dateModified'] = datetime.datetime.utcnow().isoformat()
+    #     new_mock_tender['_rev'] = '2-' + uuid.uuid4().hex
+    #     bridge.db.get = MagicMock(return_value=new_mock_tender)
+    #     tid = uuid.uuid4().hex
+    #     bridge.save_tender_in_db(tid, datetime.datetime.utcnow().isoformat())
+    #     x = log_string.getvalue().split('\n')
+    #     self.assertEqual(x[5].strip(), 'Saving tender ' + tid + ' fail with error (400, (u\'illegal_database_name\', u"Name: \'bridge.couch_url\'. Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed. Must begin with a letter."))')
+    #
+    #     logger.removeHandler(stream_handler)
+    #     log_string.close()
 
-        bridge = EdgeDataBridge(self.config)
-        mock_tender = {'data': test_tender_data}
-        bridge.client.get_tender = MagicMock(return_value=mock_tender)
+    # def test_run(self):
+    #     log_string = io.BytesIO()
+    #     stream_handler = logging.StreamHandler(log_string)
+    #     logger.addHandler(stream_handler)
+    #
+    #     bridge = EdgeDataBridge(self.config)
+    #     mock_tender = {'data': test_tender_data}
+    #     bridge.client.get_tender = MagicMock(return_value=mock_tender)
+    #     tid = uuid.uuid4().hex
+    #     t_date_modified = datetime.datetime.utcnow().isoformat()
+    #     mock_tender['data']['dateModified'] = t_date_modified
+    #     bridge.get_items_list = MagicMock(return_value=[[tid, datetime.datetime.utcnow().isoformat()]])
+    #     bridge.run()
+    #     x = log_string.getvalue().split('\n')
+    #     self.assertEqual(x[2], 'Start Edge Bridge')
+    #     self.assertEqual(x[3], 'Start data sync...')
+    #     del_tender = bridge.db.get(tid)
+    #     bridge.db.delete(del_tender)
+    #
+    #     logger.removeHandler(stream_handler)
+    #     log_string.close()
 
-        # Save tender
-
-        tid = uuid.uuid4().hex
-        t_date_modified = datetime.datetime.utcnow().isoformat()
-        mock_tender['data']['dateModified'] = t_date_modified
-        bridge.save_tender_in_db(tid, t_date_modified)
-        x = log_string.getvalue().split('\n')
-        self.assertEqual(x[1].strip(), 'Save tender ' + tid)
-        tender_in_db = bridge.db.get(tid)
-        self.assertEqual(tender_in_db.id, tid)
-
-        # Tender exist in db and not modified
-        result = bridge.save_tender_in_db(tid, t_date_modified)
-        self.assertEqual(result, None)
-
-        # Update tender
-        t_date_modified = datetime.datetime.utcnow().isoformat()
-        mock_tender['data']['dateModified'] = t_date_modified
-        bridge.save_tender_in_db(tid, t_date_modified)
-        x = log_string.getvalue().split('\n')
-        self.assertEqual(x[2].strip(), 'Update tender ' + tid)
-        updated_tender = bridge.db.get(tid)
-        self.assertEqual(updated_tender['dateModified'], unicode(t_date_modified))
-
-        # Tender not found
-        bridge.client.get_tender = MagicMock(return_value=test_tender_data)
-        bridge.save_tender_in_db(tid, datetime.datetime.utcnow().isoformat())
-        x = log_string.getvalue().split('\n')
-        self.assertEqual(x[3].strip(), 'Tender ' + tid + ' not found')
-        bridge.db.delete(updated_tender)
-
-        # Saving tender with exception
-        bridge.client.get_tender = MagicMock(return_value=mock_tender)
-        bridge.config['main']['couch_url'] = ''
-        bridge.config['main']['public_db'] = ''
-        bridge.db = Database('bridge.couch_url',
-                            session=Session(retry_delays=range(10)))
-        new_mock_tender = mock_tender
-        new_mock_tender['dateModified'] = datetime.datetime.utcnow().isoformat()
-        new_mock_tender['_rev'] = '2-' + uuid.uuid4().hex
-        bridge.db.get = MagicMock(return_value=new_mock_tender)
-        tid = uuid.uuid4().hex
-        bridge.save_tender_in_db(tid, datetime.datetime.utcnow().isoformat())
-        x = log_string.getvalue().split('\n')
-        self.assertEqual(x[5].strip(), 'Saving tender ' + tid + ' fail with error (400, (u\'illegal_database_name\', u"Name: \'bridge.couch_url\'. Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed. Must begin with a letter."))')
-
-        logger.removeHandler(stream_handler)
-        log_string.close()
-
-    def test_run(self):
-        log_string = io.BytesIO()
-        stream_handler = logging.StreamHandler(log_string)
-        logger.addHandler(stream_handler)
-
-        bridge = EdgeDataBridge(self.config)
-        mock_tender = {'data': test_tender_data}
-        bridge.client.get_tender = MagicMock(return_value=mock_tender)
-        tid = uuid.uuid4().hex
-        t_date_modified = datetime.datetime.utcnow().isoformat()
-        mock_tender['data']['dateModified'] = t_date_modified
-        bridge.save_tender_in_db(tid, t_date_modified)
-        bridge.get_teders_list = MagicMock(return_value=[[tid, datetime.datetime.utcnow().isoformat()]])
-        bridge.run()
-        x = log_string.getvalue().split('\n')
-        self.assertEqual(x[2], 'Start Edge Bridge')
-        self.assertEqual(x[3], 'Start data sync...')
-        del_tender = bridge.db.get(tid)
-        bridge.db.delete(del_tender)
-
-        logger.removeHandler(stream_handler)
-        log_string.close()
-
-    @patch('openprocurement.edge.databridge.get_tenders')
-    def test_get_tenders_list(self, mock_get_tenders):
+    @patch('openprocurement.edge.databridge.get_items')
+    def test_get_items_list(self, mock_get_items):
         tid = uuid.uuid4().hex
         t_date_modified =  datetime.datetime.utcnow().isoformat()
-        mock_get_tenders.return_value = [{'id': tid, 'dateModified': t_date_modified}]
+        mock_get_items.return_value = [{'id': tid, 'dateModified': t_date_modified}]
         bridge = EdgeDataBridge(self.config)
-        for tender_id, date_modified in bridge.get_teders_list():
+        for tender_id, date_modified in bridge.get_items_list():
             self.assertEqual(tender_id, tid)
             self.assertEqual(t_date_modified, date_modified)
 
