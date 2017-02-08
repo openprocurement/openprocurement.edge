6aff02af854d80db8feaccf6d7f1639cda826f0b
VDigitall <vitalldh@gmail.com>
2017-02-08 08:38:34 +0200

    Release v1.0.0dev3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.edge',
-      version='1.0.0dev2',
+      version='1.0.0dev3',
       description='openprocurement.edge',
       long_description=README,
       classifiers=[
