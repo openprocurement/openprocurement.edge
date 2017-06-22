1066e1a6c4cd40f097736e007ee8896145a95096
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-06-22 13:57:29 +0300

    Added status field to value for contracts views

--- openprocurement/edge/couch_views/contracts/views/test_by_dateModified/map.js
+++ openprocurement/edge/couch_views/contracts/views/test_by_dateModified/map.js
@@ -1,6 +1,6 @@
 function(doc) {
     if(doc.doc_type == 'Contract' && doc.mode == 'test') {
-        var fields=['contractID'], data={};
+        var fields=['contractID', 'status'], data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
                 data[fields[i]] = doc[fields[i]]
--- openprocurement/edge/couch_views/contracts/views/test_by_local_seq/map.js
+++ openprocurement/edge/couch_views/contracts/views/test_by_local_seq/map.js
@@ -1,6 +1,6 @@
 function(doc) {
     if(doc.doc_type == 'Contract' && doc.mode == 'test') {
-        var fields=['contractID', 'dateModified'], data={};
+        var fields=['contractID', 'status', 'dateModified'], data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
                 data[fields[i]] = doc[fields[i]]
